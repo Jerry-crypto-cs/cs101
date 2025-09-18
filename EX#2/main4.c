be7ddedb6f96bfd980eb6395d0ec87f09cd350a9
Jerry-crypto-cs <B1429013@cgu.edu.tw>
2025-09-18 15:47:08 +0800

    Create main4.c

---
+++ EX#2/main4.c
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    
+    if (a == 10) {
+        printf("a is equal to 10\n");
+    } else if (a < 20) {
+        printf("a is less than 20\n");
+    } else {
+        printf("a is not less than 20\n");
+    }
+    return 0;
+        
+}
